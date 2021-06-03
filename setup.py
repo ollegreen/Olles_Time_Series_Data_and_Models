9e2bb7fbfec936e7afb28d903326f2a8c2a179f4
Olle <51267708+ollegreen@users.noreply.github.com>
2021-06-03 21:33:41 +0100

    Start
    Cleanup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='Olles_TS_Datakit',
+      version='0.1',
+      description='Datasets to use for time series analysis.',
+      packages=['distributions'],
+      author = "Olle Green",
+      author_email = "olleg.green@gmail.com",
+    zip_safe=False)
