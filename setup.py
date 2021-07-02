f440caee5c5f9e11a5497a8b355b0b19c693553c
Olle <51267708+ollegreen@users.noreply.github.com>
2021-07-02 10:31:37 +0100

    Delete setup.py

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(name='Olles_TS_Datakit',
-      version='0.1',
-      description='Datasets to use for time series analysis.',
-      packages=['distributions'],
-      author = "Olle Green",
-      author_email = "olleg.green@gmail.com",
-    zip_safe=False)
